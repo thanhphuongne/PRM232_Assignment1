b8cf7cf081b84b12ee0cce8a716402af1064c280
thanhphuongne <lephuong14052004@gmail.com>
2025-09-29 20:56:49 +0700

    update FE with BE

--- FE/clothing-store-ui/lib/api.ts
+++ FE/clothing-store-ui/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5113';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://prn232-assignment1-t9tu.onrender.com';
 
 export interface Product {
   id: number;
